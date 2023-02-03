495fdb779a8f54fc6a6e10da7d2860edf68690e8
Ilya <il.stafeev@gmail.com>
2023-02-03 16:07:30 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from schemas import Book
+
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return {"key": "Hello"}
+
+
+@app.get('/{pk}')
+def get_item(pk: int):
+    return {"key": pk}
+
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"user": pk, "item": item}
+
+
+@app.post('/book/')
+def create_book(item: Book):
+    return item
